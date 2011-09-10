ae0c5148c9a92ee7dddc7c5d20ae4e332a5134a6
gnudist <eugenek@45-98.org>
2011-09-10 10:07:18 +0400

    Corrected example.

--- examples/7/example.pl
+++ examples/7/example.pl
@@ -1,26 +1,35 @@
 use strict;
 
+
+# 0. Clear default $dbh setting:
+
+ORM::Db -> init( $dbh );
+
+
 # 1. no init required anymore
 
 my $t = ExampleClass -> get( _dbh => $dbh,
 			     id => 123 );
 
 
-# 2. DBH is stored:
+
+# 2. Now DBH is stored:
 
 my $t1 = ExampleClass -> get( id => 456 );
 
 
 
 
+# 3. It also has been set default, but if you pass _dbh for this
+# class, it will be used and stored instead
 
-# 3. crash! no dbh stored nor supplied for this class:
-# you can still set default for all classes with ORM::Db -> init( $dbh )
-
-my $t2 = AnotherExampleClass -> get( id => 123 ); 
+my $t2 = AnotherExampleClass -> get( id => 123,
+				     _dbh => $other_dbh ); 
 
 
 
+# Remember that you need to set DBH for class only once. Or just set
+# default with first _dbh or ORM::Db -> init()
 
 # 4. Another dbh set:
 
@@ -30,5 +39,23 @@ my $t = AnotherExampleClass -> get( id => 123 ); # ok
 
 
 
+# 5. So if you write:
+
+package Class1;
+
+has 'other' => ( ...,
+                 description => { foreign_key => 'Class2' } );
+
+...
+
+$Class2::_dbh = $other_dbh;
+
+my $c = Class1 -> get( ...,
+                       _dbh => $first_dbh );
+
+$c -> other(); # selected from $other_dbh
+
+# Remember that you need to set DBH for class only once. Or just set
+# default with first _dbh or ORM::Db -> init()
 
 42;
