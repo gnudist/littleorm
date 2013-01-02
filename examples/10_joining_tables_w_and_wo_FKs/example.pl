af932d9dee866481360f17710a93edeb3ad58380
gnudist <eugenek@45-98.org>
2013-01-03 00:53:01 +0400

    Extended example a bit.

--- examples/10_joining_tables_w_and_wo_FKs/example.pl
+++ examples/10_joining_tables_w_and_wo_FKs/example.pl
@@ -1,5 +1,7 @@
 
 
+1. Tables *DO NOT* have declared FK between them:
+
 Code:
 
 M1 -> filter( field1 => M2 -> filter( ...,
@@ -8,3 +10,10 @@ M1 -> filter( field1 => M2 -> filter( ...,
 SQL:
 
 ... AND M1.field1=M2.field2 ...
+
+
+
+2. If tables *DO* have declared FK we still may just write:
+
+M1 -> filter( M2 -> filter( ... ) )
+
