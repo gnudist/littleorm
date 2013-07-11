211a9374c529b22526444dff6e60766c4c3ecf43
gnudist <eugenek@45-98.org>
2013-07-11 16:55:58 +0400

    WIP

--- ttt/scripts/run/000_dbh_is_ok.pl
+++ ttt/scripts/run/000_dbh_is_ok.pl
@@ -6,11 +6,13 @@ use lib ( "./", "../../../" );
 use ORM::Db ();
 use TestDB ();
 
-use Carp::Assert 'assert';
+use Test::More;
+
+plan tests => 1;
 
 my $dbh = &TestDB::dbconnect();
 
-assert( ORM::Db::dbh_is_ok( $dbh ) );
+ok( ORM::Db::dbh_is_ok( $dbh ), 'LittleORM likes this $dbh' );
 
 $dbh -> disconnect();
 
