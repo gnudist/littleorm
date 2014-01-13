982c06f8e8c1a733a07b38f1efd4df11bf075c9e
gnudist <eugenek@45-98.org>
2014-01-13 18:55:14 +0400

    Updated test.

--- ttt/scripts/run/033_connect_w_connector_role.pl
+++ ttt/scripts/run/033_connect_w_connector_role.pl
@@ -5,32 +5,16 @@ use lib ( "./", "../../../" );
 
 package main;
 
-use TestDB ();
-use TestDB1 ();
-
 use Test::More;
 
 use Models::BookHFwC ();
-
-use Data::Dumper 'Dumper';
-
-my $dbh = &TestDB::dbconnect();
-my $dbh1 = &TestDB1::dbconnect();
-
-
 
 my @t = Models::BookHFwC -> get_many();
 my $cnt = Models::BookHFwC -> count();
 
 is( $cnt, scalar @t, 'count match' );
 
-
-
-
 ok( 1, "didnt init ORM directly and still didnt crash" );
-
-$dbh -> disconnect();
-$dbh1 -> disconnect();
 
 done_testing();
 exit( 0 );
